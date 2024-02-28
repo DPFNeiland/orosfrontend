4aa89972037d0d5ff1572930218650a16e04e243
Rodrigo Neiland <rodrigo.naohan@gmail.com>
2024-02-27 20:45:51 -0400

    Backup

--- src/screens/telalogin/styles.ts
+++ src/screens/telalogin/styles.ts
@@ -1,5 +1,4 @@
-import { StyleSheet } from "react-native";
-import LinearGradient from "react-native-linear-gradient";
+import { StyleSheet } from "react-native";  
 
 export const styles = StyleSheet.create({
   View: {
